80a266170ed7e3be809e75415e7394640a64aaab
Omarespo-dev <omaresposito21@gmail.com>
2025-01-27 14:57:12 +0100

    2 PARTE DONE MILESTONE 1-2-3

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 const containerAll = document.querySelector(".container-all")
 
 
+
 // 2 STEP SETTO ENDPOINT
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 
@@ -17,7 +18,6 @@ axios.get(endpoint)
         
         // RICAVO I DATA DALL ENDPOINT
         const data = response.data
-        // console.log(data)
 
         // PER RICAVARE GLI OGGETTI ALL INTERNO DELL ARRAY POSSO USARE O UN CICLO FOR OPPURE UN FOREACH
         data.forEach(object => {
@@ -30,7 +30,7 @@ axios.get(endpoint)
                     <img src="./img/pin.svg" alt="">
                 </div>
                 
-                 <img src="${object.url}" alt="${object.title}">
+                 <img src="${object.url}" alt="${object.title}" class="img">
                 
                 <div class="container-text">
                     <p>${object.date}</p>
@@ -38,9 +38,57 @@ axios.get(endpoint)
                 </div>
                 
             </div>`
-            
+        
         });
 
+
+        
+        
+        
+        
+        
+        // BONUS MILESTONE 1-2-3
+
+        // DICHIARO UNA VARIABILE DOVE MI PRENDE LA NODELIST DI IMMAGINI DANDOGLI UNA CLASSE IMG --- RICAVANDOLA DAL DOM
+        const cardAll = document.querySelectorAll(".img")
+        console.log(cardAll)
+
+        // DICHIARO UNA VARIABILE DOVE ALL INTERNO ANDRANNO GLI ELEMENTI DI OUTPUT -------RICAVANDOLA DAL DOM
+        const all = document.querySelector(".all")
+        
+
+
+        // CICLO LA NODELIST DI IMMAGINI COSI AD OGNI IMMAGINE DELLA NODELIST HA UN EVENT LISTENER CHE AL CLICK COMPARE
+        cardAll.forEach(object => {
+            
+            // POI DO L EVENTO AL CLICK DELL OBJECT 
+            object.addEventListener("click", function (){
+                
+                // POI GLI DICO DI PRENDERE L ELEMENTO DI OUTPUT E DI RIMUOVERGLI LA CLASSE CHE HA IL DISPLAY NONE
+                all.classList.remove("none")
+            
+                // POI SEMPRE NELL OUTPUT GLI DICO DI SCRIVERE LA PARTE IN HTML MA IN MODO DINAMICO COSI CHE IMG CORRISPONDE ALL SRC DI OGNI IMG ALL INTERNO DELLA NODELIST 
+                all.innerHTML =`
+                <div class="containerImg">
+                    <button id="bottone">CHIUDI</button>
+                    
+                    <div class="image">
+                        <img src="${object.src}" alt=""> 
+                    </div>  
+                </div>
+                `
+                // DICHIARO LA VARIABILE DEL BUTTON ----- RICAVANDOLA DAL DOM
+                const button = document.getElementById("bottone")
+
+                // INFINE DO UN ADDEVENTLISTENER AL BUTTON DICENDOGLI CHE AL CLICK AGGIUNGE LA CLASSE NONE CHE AL SUO INTERNO STA IN DISPLAY NONE
+                button.addEventListener("click", function (){
+                    all.classList.add("none")
+                });
+
+            })
+
+            
+        });
 
         
     })
@@ -50,3 +98,6 @@ axios.get(endpoint)
         //CODICE DA ESEGUIRE IN CASO DA ERRORE
         console.error(error)
     })
+    
+    
+    
